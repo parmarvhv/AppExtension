fa928706c8d8182429408a84e760702c2061321d
Vaibhav Parmar <vaibhav@nickelfox.com>
2020-02-25 19:56:24 +0530

    initial commit

---
+++ AppExtension/Code/Configuration.swift
@@ -0,0 +1,13 @@
+//
+//  Configuration.swift
+//  AppExtension
+//
+//  Created by Ravindra Soni on 11/12/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+import Foundation
+
+struct Configuration {
+	static let AppCenterAppSecret = "1234e"
+}
---
+++ AppExtension/Resources/AppExtension-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  AppExtension-Bridging-Header.h
+//  AppExtension
+//
+//  Created by Ravindra Soni on 30/11/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#ifndef AppExtension_Bridging_Header_h
+#define AppExtension_Bridging_Header_h
+
+
+#endif /* AppExtension_Bridging_Header_h */
