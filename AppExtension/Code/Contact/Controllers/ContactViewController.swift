334ada6b98d30f086a8fb91147e1657103a51736
Vaibhav Parmar <vaibhav@nickelfox.com>
2020-02-28 11:09:47 +0530

    feat(contact): sync contact from PhoneBook and persist in core data

--- AppExtension/Code/Contact/Controllers/ContactViewController.swift
+++ AppExtension/Code/Contact/Controllers/ContactViewController.swift
@@ -7,18 +7,45 @@
 //
 
 import UIKit
+import ReactiveSwift
+import ReactiveCocoa
+import Model
+import FLUtilities
+
+protocol ContactViewControllerProtocol: class {
+    var disposable: CompositeDisposable { get }
+    var loading: MutableProperty<Bool> { get }
+    var sectionModels: MutableProperty<[SectionModel]> { get }
+    var sectionCount: Int { get }
+    func rowsCount(at section: Int) -> Int
+    func cellModel(at indexPath: IndexPath) -> Any
+}
 
 class ContactViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
     
+    var viewModel: ContactViewControllerProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.initialSetup()
     }
+    
+    deinit {
+        self.viewModel.disposable.dispose()
+    }
 
     private func initialSetup() {
+        self.viewModel = ContactViewModel(self)
         self.setupTableView()
+        
+        self.viewModel.disposable += self.viewModel.sectionModels.signal.observeValues { _ in
+            DispatchQueue.main.async {            
+                self.tableView.reloadData()
+            }
+        }
+        
     }
     
 }
@@ -31,11 +58,20 @@ extension ContactViewController: UITableViewDataSource {
         self.tableView.delegate = self
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.viewModel.sectionCount
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return self.viewModel.rowsCount(at: section)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier:
+            ContactTableCell.defaultReuseIdentifier) as? TableViewCell {
+            cell.configure(self.viewModel.cellModel(at: indexPath))
+            return cell
+        }
         return UITableViewCell()
     }
     
@@ -43,5 +79,26 @@ extension ContactViewController: UITableViewDataSource {
 
 // MARK: UITableViewDelegate
 extension ContactViewController: UITableViewDelegate {
+    
+}
+
+// MARK: UITableViewDelegate
+extension ContactViewController: ContactViewModelProtocol {
+    
+    func showOpenSettingsAlert() {
+        
+        let message = "Damn, you need to give me access of your contacts to that I can scan them all."
+        
+        let interfaceList = [ActionInterface(title: "No"), ActionInterface(title: "Yes")]
+        self.showAlertController(title: "Contact Access",
+                                 message: message,
+                                 preferredStyle: .alert,
+                                 actionInterfaceList: interfaceList) { (interface) in
+            if interface.title == "Yes" {
+                let settingsString = UIApplication.openSettingsURLString
+                UIApplication.shared.open(URL(string: settingsString)!)
+            }
+        }
+    }
     
 }
