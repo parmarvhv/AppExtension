8d5a044d8cb48ed93adf2e3629696b612b57d39d
Vaibhav Parmar <vaibhav@nickelfox.com>
2020-03-18 10:38:32 +0530

    chore(firebase): don't initilise firebase and AppCenter analytics

--- AppExtension/Code/AppDelegate.swift
+++ AppExtension/Code/AppDelegate.swift
@@ -22,8 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        self.setupAppCenterAnalytics()
-        self.registerFirebase()
+        //self.setupAppCenterAnalytics()
+        //self.registerFirebase()
         return true
     }
 
